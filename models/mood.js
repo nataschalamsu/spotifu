eb6cbdd8d74601775c332eb30baf7b90e9f2f41e
fastabiqul1993 <fastabiqul1993@gmail.com>
2018-04-26 21:09:01 +0700

    finish search 2

--- models/mood.js
+++ models/mood.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const Sequelize = require('sequelize')
-const Op = Sequelize.Op
+// const Op = Sequelize.Op
 
-const Song = require('./song')
+// const Song = require('./song')
+// console.log(Song)
 
 module.exports = (sequelize, DataTypes) => {
   var Mood = sequelize.define('Mood', {
@@ -16,18 +17,56 @@ module.exports = (sequelize, DataTypes) => {
     // Mood.hasMany(models.Song)
   };
 
-  Mood.prototype.getMood = function(param) {
+  // Mood.prototype.getMood = function(param) {
 
-    Mood.findall({
-      include: [Song]
-    })
-    .then(songs => {
-      return songs
-    })
-    .catch(err => {
-      return err
-    })
-  }
+  //   Mood.findall({
+  //     include: [Song]
+  //   })
+  //   .then(songs => {
+  //     return songs
+  //   })
+  //   .catch(err => {
+  //     return err
+  //   })
+  // }
+  // Mood.getMood = function(param) {
+
+  //   return new Promise(function(resolve, reject){
+  //     Mood.findAll({
+  //       include: [Song],
+  //       where: {
+  //         mood: {
+  //           [Op.like]: `%${param}%`
+  //         }
+  //       }
+  //     })
+  //     .then(songs => {
+  //       resolve(songs)
+  //     })
+  //     .catch(err => {
+  //       reject(err)
+  //     })
+  //   })
+  // }
+
+  // Song.getSongsByTitle = function(param) {
+    
+  //   return new Promise(function(resolve, reject){
+  //     Song.findAll({
+  //       where : {
+  //         title_song: {
+  //           [Op.like]: `%${param}%`
+  //         }
+  //       }
+  //     })
+  //       .then(songs => {
+  //         resolve(songs)
+  //       })
+  //       .catch(err => {
+  //         reject(err)
+  //       })
+  //   })
+  // }
 
   return Mood;
 };
