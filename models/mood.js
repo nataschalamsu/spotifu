ca021a2492a2c8ffa41f30b421a5c9f96009f9b6
Natascha Lamsu <tazcha15@gmail.com>
2018-04-27 09:16:26 +0700

    models views image

--- models/mood.js
+++ models/mood.js
@@ -17,56 +17,5 @@ module.exports = (sequelize, DataTypes) => {
     // Mood.hasMany(models.Song)
   };
 
-  // Mood.prototype.getMood = function(param) {
-
-  //   Mood.findall({
-  //     include: [Song]
-  //   })
-  //   .then(songs => {
-  //     return songs
-  //   })
-  //   .catch(err => {
-  //     return err
-  //   })
-  // }
-  // Mood.getMood = function(param) {
-
-  //   return new Promise(function(resolve, reject){
-  //     Mood.findAll({
-  //       include: [Song],
-  //       where: {
-  //         mood: {
-  //           [Op.like]: `%${param}%`
-  //         }
-  //       }
-  //     })
-  //     .then(songs => {
-  //       resolve(songs)
-  //     })
-  //     .catch(err => {
-  //       reject(err)
-  //     })
-  //   })
-  // }
-
-  // Song.getSongsByTitle = function(param) {
-
-  //   return new Promise(function(resolve, reject){
-  //     Song.findAll({
-  //       where : {
-  //         title_song: {
-  //           [Op.like]: `%${param}%`
-  //         }
-  //       }
-  //     })
-  //       .then(songs => {
-  //         resolve(songs)
-  //       })
-  //       .catch(err => {
-  //         reject(err)
-  //       })
-  //   })
-  // }
-
   return Mood;
 };
